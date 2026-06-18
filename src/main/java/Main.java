c3fe71883eaeafe280e79356d0d37ecd74593f54
RaghveshSharma <raghvesh18.s@gmail.com>
2026-06-18 11:34:38 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        
-        
-        while (true){
+
+        while (true) {
             System.out.print("$ ");
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
+
+            // Check if the user wants to terminate the shell
+            if (input.equals("exit 0") || input.equals("exit")) {
+                break;
+            }
+
             System.out.println(input + ": command not found");
         }
+        sc.close();
     }
 }
